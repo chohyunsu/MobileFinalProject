8ac35f0ba65d186166ea8b303dcdbd7d9a9b4c8e
hyun su cho <chohyunsu1996@gmail.com>
2020-12-17 23:02:31 +0900

    SNSProject
    Mobile Final Project : SNS Project

---
+++ Android_SNS/app/src/main/java/com/example/sns_project/listener/OnPostListener.java
@@ -0,0 +1,9 @@
+package com.example.sns_project.listener;
+
+import com.example.sns_project.PostInfo;
+
+public interface OnPostListener {
+
+    void onDelete(PostInfo postInfo);
+    void onModify();
+}
